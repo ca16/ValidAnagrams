e5a899d3270d43abfe629b8e22bccdecf14a1e72
ca16 <chloeanastasiades@gmail.com>
2016-10-13 15:34:28 -0700

    added to dict constructor test

--- src/tests/java/vaf/DictRepConstructorTest.java
+++ src/tests/java/vaf/DictRepConstructorTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import vaf.mapversion.Sorter;
 import vaf.trieversion.Trie;
 
 import static vaf.PathsAndNames.PATH_TO_B_DICT;
@@ -16,18 +18,18 @@ import static vaf.PathsAndNames.PATH_TO_S_DICT;
  * Created by Chloe on 10/8/16.
  */
 public class DictRepConstructorTest {
-    
+
+    List<String> smallDictContents;
+    List<String> otherWords;
+
+
+
     @Before
     public void setUp() throws Exception {
 
         PathsAndNames.populatePaths();
-    }
 
-    @Test
-    public void constructTrie() throws Exception {
-        
-        
-        List<String> smallDictContents = new ArrayList<>();
+        smallDictContents = new ArrayList<>();
         smallDictContents.add("pumpkin");
         smallDictContents.add("beverage");
         smallDictContents.add("light");
@@ -39,22 +41,11 @@ public class DictRepConstructorTest {
         smallDictContents.add("pickle");
         smallDictContents.add("bats");
         smallDictContents.add("stab");
-        smallDictContents.add("canopy");        
+        smallDictContents.add("canopy");
         smallDictContents.add("cereal");
         smallDictContents.add("globe");
-        
-        Trie smallDictComp = DictRepConstructor.constructTrie(PATH_TO_S_DICT);
-        
-        for (String word : smallDictContents){
-            Assert.assertTrue(smallDictComp.contains(word));
-        }
 
-        Trie bigDictComp = DictRepConstructor.constructTrie(PATH_TO_B_DICT);
-
-        for (String word : smallDictContents){
-            Assert.assertTrue(bigDictComp.contains(word));
-        }        
-        List<String> otherWords = new ArrayList<>();
+        otherWords = new ArrayList<>();
         otherWords.add("yellow");
         otherWords.add("fog");
         otherWords.add("sea");
@@ -69,6 +60,24 @@ public class DictRepConstructorTest {
         otherWords.add("beetle");
         otherWords.add("caring");
         otherWords.add("acted");
+    }
+
+    @Test
+    public void constructTrie() throws Exception {
+        
+        
+        Trie smallDictComp = DictRepConstructor.constructTrie(PATH_TO_S_DICT);
+        
+        for (String word : smallDictContents){
+            Assert.assertTrue(smallDictComp.contains(word));
+        }
+
+        Trie bigDictComp = DictRepConstructor.constructTrie(PATH_TO_B_DICT);
+
+        for (String word : smallDictContents){
+            Assert.assertTrue(bigDictComp.contains(word));
+        }        
+
 
 
         for (String word : otherWords){
@@ -83,6 +92,38 @@ public class DictRepConstructorTest {
         Assert.assertFalse(bigDictComp.contains("ddddd"));
         Assert.assertFalse(bigDictComp.contains("owiwewd"));
         Assert.assertFalse(bigDictComp.contains("slddng"));
+
+
+    }
+
+    @Test
+    public void constructMap() throws Exception {
+
+        Map smallDictComp = DictRepConstructor.constructMap(PATH_TO_S_DICT);
+
+        for (String word : smallDictContents){
+            Assert.assertTrue(smallDictComp.containsKey(Sorter.sortWord(word)));
+        }
+
+        Map bigDictComp = DictRepConstructor.constructMap(PATH_TO_B_DICT);
+
+        for (String word : smallDictContents){
+            Assert.assertTrue(bigDictComp.containsKey(Sorter.sortWord(word)));
+        }
+
+
+        for (String word : otherWords){
+            Assert.assertTrue(bigDictComp.containsKey(Sorter.sortWord(word)));
+        }
+
+        for (String word : otherWords){
+            Assert.assertFalse(smallDictComp.containsKey(Sorter.sortWord(word)));
+        }
+
+        Assert.assertFalse(bigDictComp.containsKey(Sorter.sortWord("gzgzgzgz")));
+        Assert.assertFalse(bigDictComp.containsKey(Sorter.sortWord("ddddd")));
+        Assert.assertFalse(bigDictComp.containsKey(Sorter.sortWord("owiwewd")));
+        Assert.assertFalse(bigDictComp.containsKey(Sorter.sortWord("slddng")));
 
 
     }
